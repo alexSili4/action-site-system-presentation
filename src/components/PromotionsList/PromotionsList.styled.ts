f0381b09aef9632e2de5c8624e84f0947eede4dc
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-10-01 16:53:44 +0300

    add banners

--- src/components/PromotionsList/PromotionsList.styled.ts
+++ src/components/PromotionsList/PromotionsList.styled.ts
@@ -1,4 +1,7 @@
 import styled from '@emotion/styled';
+import firstBanner from '@/images/promotions/banners/first-banner.jpg';
+import secondBanner from '@/images/promotions/banners/second-banner.jpg';
+import thirdBanner from '@/images/promotions/banners/third-banner.jpg';
 
 export const Container = styled.div`
   margin-top: ${({ theme }) => theme.spacing(4)};
@@ -22,14 +25,30 @@ export const List = styled.ul`
   }
 `;
 
-export const ListItem = styled.li``;
+export const ListItem = styled.li`
+  &:nth-child(1) > a > div {
+    background-image: url(${firstBanner});
+  }
+
+  &:nth-child(2) > a > div {
+    background-image: url(${secondBanner});
+  }
+
+  &:nth-child(3) > a > div {
+    background-image: url(${thirdBanner});
+  }
+`;
 
 export const Card = styled.div`
+  height: 492px;
   background-color: ${({ theme }) => theme.colors.lightGrey};
   border-radius: 8px;
   padding: ${({ theme }) => `${theme.spacing(8)} ${theme.spacing(6)}`};
+  background-size: cover;
+  background-position: center;
 
   @media (min-width: ${({ theme }) => theme.breakpoints.desktop}px) {
+    height: 485px;
     padding: ${({ theme }) => theme.spacing(14)};
   }
 `;
